e2ec9e62517ea78f4e7a804a916877528157d93a
Tiya Jain <tiyajain2002@gmail.com>
2023-03-09 22:09:07 +0530

    memories initialized

--- src/myRISCVSim.py
+++ src/myRISCVSim.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 x=[0]*32
 x[2]=int("0x7FFFFFF0",16) #sp - stack pointer
 x[3]=int("0x10000000",16) #the beginning address of data segment of memory
@@ -5,3 +7,10 @@ x[3]=int("0x10000000",16) #the beginning address of data segment of memory
 clk=0
 
 RS1,RS2,RD,RM,RZ,RY,RA,RB,PC,IR,MuxB_select,MuxC_select,MuxINC_select,MuxY_select,MuxPC_select,MuxMA_select,RegFileAddrA,RegFileAddrB,RegFileAddrC,RegFileInp,RegFileAOut,RegFileBOut,MAR,MDR,opcode,numBytes,RF_Write,immed,PC_Temp,Mem_Write,Mem_Read=[0]*31
+
+ALUOp=[0]*26
+
+data_memory= defaultdict()
+instruction_memory=defaultdict()
+
+
