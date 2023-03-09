cc93ffef4eb39ffc6c3a63b8855e05962d22ecc4
Tiya Jain <tiyajain2002@gmail.com>
2023-03-09 21:50:37 +0530

    Co-authored-by: AkashGarg044 <AkashGarg044@users.noreply.github.com>

--- src/myRISCVSim.py
+++ src/myRISCVSim.py
@@ -1,3 +1,7 @@
 x=[0]*32
-x[2]=int("0x7FFFFFF0",16)
-x[3]=int("0x10000000",16)
+x[2]=int("0x7FFFFFF0",16) #sp - stack pointer
+x[3]=int("0x10000000",16) #
+
+clk=0
+
+RS1,RS2,RD,RM,RZ,RY,RA,RB,PC,IR,MuxB_select,MuxC_select,MuxINC_select,MuxY_select,MuxPC_select,MuxMA_select,RegFileAddrA,RegFileAddrB,RegFileAddrC,RegFileInp,RegFileAOut,RegFileBOut,MAR,MDR,opcode,numBytes,RF_Write,immed,PC_Temp,Mem_Write,Mem_Read=[0]*31
