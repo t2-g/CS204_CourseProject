74eb5bcbd68746fdb7e027949c9a6914c86e849e
Tiya Jain <tiyajain2002@gmail.com>
2023-03-10 22:12:41 +0530

    changed

--- src/myRISCVSim.py
+++ src/myRISCVSim.py
@@ -6,9 +6,10 @@ x[3]=int("0x10000000",16) #the beginning address of data segment of memory
 
 clk=0
 
-RS1,RS2,RD,RM,RZ,RY,RA,RB,PC,IR,MuxB_select,MuxC_select,MuxINC_select,MuxY_select,MuxPC_select,MuxMA_select,RegFileAddrA,RegFileAddrB,RegFileAddrC,RegFileInp,RegFileAOut,RegFileBOut,MAR,MDR,opcode,numBytes,RF_Write,immed,PC_Temp,Mem_Write,Mem_Read=[0]*31
+RS1,RS2,RD,PC,IR,MuxB_select,_MuxC_select,MuxINC_select,_MuxY_select,MuxPC_select,MuxMA_select,RegFileAddrA,RegFileAddrB,RegFileAddrC,RegFileInp,RegFileAOut,RegFileBOut,MAR,MDR,opcode,numBytes,RF_Write,immed,PC_Temp,Mem_Write,Mem_Read=[0]*31
 #MuxB_select==op2select
-#MuxMA_select==
+#MuxINC_select==IsBranch
+
 ALUOp=[0]*26
 
 data_memory= defaultdict()
